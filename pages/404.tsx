bc6f06b283072b7a84d1b51f07e1b38c80cc8ade
edu-as <edu-as@hotmail.com>
2022-11-13 20:45:18 -0300

    Style: Changed dynamic text fields

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,17 @@
-import LayoutError from '../layouts/404';
+import LayoutError from "../layouts/404";
 
 const ErrorPage = () => (
   <LayoutError>
     <section className="error-page">
       <div className="container">
         <h1>Error 404</h1>
-        <p>Woops. Looks like this page doesn't exist</p>
-        <a href="#" className="btn btn--rounded btn--yellow">Go to home</a>
+        <p>Ops. Parece que esta página não existe</p>
+        <a href="#" className="btn btn--rounded btn--yellow">
+          Ir para home
+        </a>
       </div>
     </section>
   </LayoutError>
-)
+);
 
-export default ErrorPage
+export default ErrorPage;
--- utils/data/products-sizes.ts
+++ utils/data/products-sizes.ts
@@ -1,28 +1,28 @@
 export const productsSizes = [
-    {
-      id: '1',
-      label: 'XS',
-    },
-    {
-      id: '2',
-      label: 'S',
-    },
-    {
-      id: '3',
-      label: 'M',
-    },
-    {
-      id: '4',
-      label: 'L',
-    },
-    {
-      id: '5',
-      label: 'XL',
-    },
-    {
-      id: '6',
-      label: 'XXL',
-    },
-  ];
-  
-  export default productsSizes;
+  {
+    id: "1",
+    label: "P",
+  },
+  {
+    id: "2",
+    label: "M",
+  },
+  {
+    id: "3",
+    label: "G",
+  },
+  {
+    id: "4",
+    label: "GG",
+  },
+  {
+    id: "5",
+    label: "XG",
+  },
+  {
+    id: "6",
+    label: "XXG",
+  },
+];
+
+export default productsSizes;
